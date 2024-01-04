c14729387cd7d1dab66fb677183fdde7a32c7f22
kudakwashe chris chipangura <49289670+kudah99@users.noreply.github.com>
2024-01-04 04:07:33 +0200

    updated a package

--- app/settings.py
+++ app/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'tailwind',
+    'django-tailwind',
     'django_browser_reload',
 
     'home',

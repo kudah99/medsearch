7f62a2ec5f7673ee9fbe7906eb207fe926f29b48
kudakwashe chris chipangura <49289670+kudah99@users.noreply.github.com>
2024-01-04 04:24:08 +0200

    fix vercel error

--- app/settings.py
+++ app/settings.py
@@ -37,7 +37,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django-tailwind',
     'django_browser_reload',
 
     'home',

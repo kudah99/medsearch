48f4b9d410faf5149b2e6bb42c399a3757897c9a
kudakwashe chris chipangura <49289670+kudah99@users.noreply.github.com>
2024-01-07 04:23:19 +0200

    add static path

--- app/urls.py
+++ app/urls.py
@@ -15,21 +15,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include,re_path
-from django.views.generic.base import RedirectView
-from django.views.generic import TemplateView
+from django.urls import path, include
 from django.conf import settings
-from django.conf.urls.static import static,serve
-
-favicon_view = RedirectView.as_view(url='/media/images/favicon.ico', permanent=True)
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("__reload__/", include("django_browser_reload.urls")),
     #path("", TemplateView.as_view(template_name="base.html")),
-    path('', include('home.urls')),
-    re_path(r'^media/images/favicon\.ico$', favicon_view),
-    re_path(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
+    path('', include('home.urls'))
 ]
-
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

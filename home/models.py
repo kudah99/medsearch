bbfa8193754952b309b1767cedcf9fdd87fc8c73
kudakwashe chris chipangura <49289670+kudah99@users.noreply.github.com>
2024-01-09 03:24:44 +0200

    added city model

--- home/models.py
+++ home/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class City(models.Model):
+    name = models.CharField(max_length=100)
+    latitude = models.DecimalField(max_digits=9, decimal_places=6)
+    longitude = models.DecimalField(max_digits=9, decimal_places=6)
+
+    def __str__(self):
+        return self.name
+    
+

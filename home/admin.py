8fe07256517fc9538b06791fa78bf8fe110e71f8
kudakwashe chris chipangura <49289670+kudah99@users.noreply.github.com>
2024-01-09 03:50:03 +0200

    added admin.py

--- home/admin.py
+++ home/admin.py
@@ -1,1 +1,8 @@
+# zimbabwe_cities/admin.py
 from django.contrib import admin
+from import_export.admin import ImportExportModelAdmin
+from .models import City
+
+@admin.register(City)
+class CityAdmin(admin.ModelAdmin):
+    pass
--- home/views.py
+++ home/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render, get_object_or_404,redirect
+from .models import City
 
 
 def index(request):
-    return render(request, 'index.html')
+    cities = City.objects.all()
+    context = {'cities': cities}
+    return render(request, 'index.html',context=context)

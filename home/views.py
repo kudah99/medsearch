05f4bb2f03b80d6e349feec83a7ee655e1232c53
kudakwashe chris chipangura <49289670+kudah99@users.noreply.github.com>
2024-01-15 01:28:48 +0200

    added about us section

--- home/views.py
+++ home/views.py
@@ -1,15 +1,16 @@
 from django.shortcuts import render, get_object_or_404,redirect
-from .models import City, HealthFacility,HealthFacilityCategory,HealthSpecialist
+from .models import City, HealthFacility,HealthFacilityCategory,HealthSpecialist,MedicalSpecialty
 
 
 def index(request):
     cities = City.objects.all()
-
-
     category = HealthFacilityCategory.objects.get(id=1)
     mental_health_facilities  = HealthFacility.objects.filter(category=category)
-    context = {'cities': cities, 'mental_health_facilities': mental_health_facilities}
+    medical_specialties = MedicalSpecialty.objects.all()
+    context = {'cities': cities, 'mental_health_facilities': mental_health_facilities, 'medical_specialties': medical_specialties}
+
     return render(request, 'index.html',context=context)
+
 
 def search_specialists(request):
     if request.method == 'POST':

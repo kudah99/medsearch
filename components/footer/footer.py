ce30f65041f8936e1857eddc9f21df4e6ec59bd2
kudakwashe chris chipangura <49289670+kudah99@users.noreply.github.com>
2024-01-15 03:02:24 +0200

    added footer

--- components/footer/footer.py
+++ components/footer/footer.py
@@ -4,11 +4,6 @@ from django_components import component
 class Footer(component.Component):
     template_name = "footer/template.html"
 
-    def get_context_data(self, value):
-        return {
-            "param": "sample value",
-        }
-
     class Media:
         css = "footer/style.css"
         js = "footer/script.js"

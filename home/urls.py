357dca98e566a1f8163825bfa508264fd770b260
kudakwashe chris chipangura <49289670+kudah99@users.noreply.github.com>
2024-01-22 16:32:06 +0200

    vercel error fix

--- home/urls.py
+++ home/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.index, name='home'),
     path('search/',views.search_specialists, name='search_specialists'),
      path("medical_specialist/", views.health_specialist, name="medical-specialist"),
+     path("health_facility/", views.health_facility, name="health_facility"),
 ]
--- home/views.py
+++ home/views.py
@@ -26,6 +26,18 @@ def search_specialists(request):
         )
         return render(request, f'health_specialists.html', {'results': results})
 
+def search_specialists(request):
+    if request.method == 'POST':
+        medical_specialty = request.POST.get('medical_specialty')
+        city = request.POST.get('city')
+
+        # Perform the search based on the form input
+        results = HealthSpecialist.objects.filter(
+            medical_specialty__name__icontains=medical_specialty,
+            city__id=city
+        )
+        return render(request, f'health_specialists.html', {'results': results})
+
 def health_specialist(request):
 
     # Get query parameters from the request
@@ -50,3 +62,28 @@ def health_specialist(request):
 
     context = {'health_specialists': health_specialists,'cities': cities,'health_facilities':health_facilities,'medical_specialties': medical_specialties}
     return render(request, 'health_specialist_list.html', context=context)
+
+def health_facility(request):
+
+    # Get query parameters from the request
+    full_name = request.GET.get('full_name', default='')
+    city_id = request.GET.get('city', default='')
+    medical_specialty_id = request.GET.get('medical_specialty', default='')
+    cities = City.objects.all()
+    health_facilities  = HealthFacility.objects.all()
+    medical_specialties = MedicalSpecialty.objects.all()
+
+    # Build a filter based on the query parameters
+    filter_args = Q()
+    if full_name:
+        filter_args &= Q(full_name__icontains=full_name)
+    if city_id:
+        filter_args &= Q(city__id=city_id)
+    if medical_specialty_id:
+        filter_args &= Q(medical_specialty__id=medical_specialty_id)
+
+    # Apply the filter to the queryset
+    health_specialists = HealthSpecialist.objects.filter(filter_args)
+
+    context = {'health_specialists': health_specialists,'cities': cities,'health_facilities':health_facilities,'medical_specialties': medical_specialties}
+    return render(request, 'health_facility_list.html', context=context)
